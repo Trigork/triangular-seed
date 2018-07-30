f1bde78f8868dba7a2258139b84f4d4b9e40de14
unknown <trigork@gmail.com>
2018-07-30 18:47:47 +0200

    removed bower, fixed vulns

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,5 +1,6 @@
+
 angular.module('mainCtrl', [])
-.controller('mainController', function($rootScope, $location) { 
+.controller('mainController', function($rootScope, $location) {
 	var vm = this;
 	vm.message = 'HI THERE!';
 });
